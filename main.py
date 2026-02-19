5eff3f02989f13c7be164ed8b311d91727e631ad
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-19 12:02:05 -0600

    ajuste final despues del rebase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print("Cambio desde main")
 print("Cambio desde A")
-<<<<<<< HEAD
 print ("cambio desde main otra vez y no rebase")
-=======
 print("Cambio desde rebase")
->>>>>>> 30645a4 (cambio en feature-rebase)
