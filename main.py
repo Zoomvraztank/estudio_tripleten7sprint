220f0fe399f359321b80cc6acbe03886af4fce38
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-19 11:52:30 -0600

    cambio en feature-rebase

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Cambio desde main")
 print("Cambio desde A")
+<<<<<<< HEAD
 print ("cambio desde main otra vez y no rebase")
+=======
+print("Cambio desde rebase")
+>>>>>>> 30645a4 (cambio en feature-rebase)
