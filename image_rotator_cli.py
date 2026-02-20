a6957b2eb2c447356336cad6dc83153025b9daf7
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-20 09:15:04 -0600

    se añade el flag --info para imprimir tamaño solo cuando se solicite

--- image_rotator_cli.py
+++ image_rotator_cli.py
@@ -8,11 +8,14 @@ parser = argparse.ArgumentParser(
 parser.add_argument("input_file", help="Ruta de la imagen de entrada")
 parser.add_argument("output_file", help="Ruta de la imagen de salida")
 parser.add_argument("--angle", "-a", type=int, default=90, help="Ángulo de rotación en grados")
+parser.add_argument("-i", "--info", action="store_true", help="Muestra las dimensiones de la imagen")
 
 args = parser.parse_args()
 
 im = Image.open(args.input_file)
-print(im.size)
 
-rotated = im.rotate(args.angle,expand=True)
+if args.info:
+    print("dimensiones:", im.size)
+
+rotated = im.rotate(args.angle, expand=True)
 rotated.save(args.output_file)
