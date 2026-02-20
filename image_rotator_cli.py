916936e0be3f57252f05b65bdb3f97f871c6fe10
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-19 23:33:35 -0600

    se mejoro el UX del script image_rotator_cli.py

--- image_rotator_cli.py
+++ image_rotator_cli.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    description="Rotador de imagen CLI usando Pillow"
+)
 
-parser.add_argument("input_file")
-parser.add_argument("output_file")
-parser.add_argument("--angle", "-a", type=int, default=90)
+parser.add_argument("input_file", help="Ruta de la imagen de entrada")
+parser.add_argument("output_file", help="Ruta de la imagen de salida")
+parser.add_argument("--angle", "-a", type=int, default=90, help="Ángulo de rotación en grados")
 
 args = parser.parse_args()
 
 im = Image.open(args.input_file)
 print(im.size)
 
-rotated = im.rotate(args.angle)
+rotated = im.rotate(args.angle,expand=True)
 rotated.save(args.output_file)
