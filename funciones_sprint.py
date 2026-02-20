518996aa9468a755854d4f57ac55a144845c517a
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-20 09:28:25 -0600

    se agregaron ejemplos de funciones que usan variables locales y globales

---
+++ funciones_sprint.py
@@ -0,0 +1,10 @@
+product_price = 12.0
+product_qty = 5
+
+def total_price():
+    product_price = 11.0
+    product_qty = 4
+    print("Dentro de la funcion:",product_price * product_qty)
+
+total_price()
+print("Fuera de la funcion:",product_price * product_qty)
---
+++ funciones_sprint_sinlocales.py
@@ -0,0 +1,7 @@
+product_price = 12.0
+product_qty = 5
+
+def total_price():
+    print("Dentro:", product_price * product_qty)
+
+total_price()
