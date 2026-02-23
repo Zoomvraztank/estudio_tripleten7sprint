bd94733987f4c15797280602d1c4dc3f3ab8d343
Zoomvraztank <raulproyecto2.0qgmail.com>
2026-02-23 13:37:45 -0600

    Agregamos la carpeta mi app con requirements de nuevo un app.py para abrir desde streamlit

--- astro.py
+++ astro.py
@@ -1,2 +1,8 @@
+print ("astro ejecutado")
+
 def saludar():
-    print("¡Hola! Bienvenido a Astro.")
+    print("¡Hola! Desde Astro.")
+
+if __name__ == "__main__":
+    print("astro ejecutado directamente")
+    saludar()
---
+++ mi_app/app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.header("Hola Streamlit")
+st.write("Si ves esto, la estructura esta funcionando")
